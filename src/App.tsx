f7e705e7b49b828c75029725cd926bbadf4aa3e5
Rafael Tosta <rafa.ecomp@gmail.com>
2024-05-14 11:33:12 -0300

    Install Flowbite

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold hover:text-blue-500 underline">
+    <h1 className="text-3xl font-bold hover:text-blue-500 dark:text-white underline dark:bg-black">
       Hello world!
     </h1>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'media',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
