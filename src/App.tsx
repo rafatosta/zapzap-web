0f36283525c88eb6457b1fb88dbd9a4d542fa9dd
Rafael Tosta <rafa.ecomp@gmail.com>
2024-05-14 15:29:04 -0300

    Create NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import NavBar from './components/NavBar'
+
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold hover:text-blue-500 dark:text-white underline dark:bg-black">
-      Hello world!
-    </h1>
+    <>
+      <NavBar />
+    </>
   )
 }
 
