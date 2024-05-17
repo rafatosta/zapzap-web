cab9049bb6fe4be7c9fbe57ed213f26ef0a55fde
Rafael Tosta <rafa.ecomp@gmail.com>
2024-05-17 20:01:36 -0300

    Update Features.tsx

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -5,26 +5,29 @@ import { Element } from 'react-scroll'
 function Features() {
     return (
         <Element name="#features">
-            <div className="h-screen mx-auto bg-[#202C33] flex flex-col justify-center items-center gap-8">
-                <div className="flex flex-col justify-center items-center w-2/3 gap-y-4">
-                    <p className="text-[#F0F2F5] text-4xl lg:text-6xl font-extrabold">
-                        {featuresTitle}
-                    </p>
-                    <p className="text-[#F0F2F5] text-base lg:text-xl text-center md:w-2/3 lg:w-2/5">
-                        {featuresSub}
-                    </p>
-                </div>
+            <div className="relative h-screen mx-auto bg-[url('/background.webp')] bg-[#202C33] flex flex-col justify-center items-center gap-8">
+                <div className="absolute inset-0 bg-[#202C33]/95 "></div>
+                <div className="relative flex flex-col justify-center items-center w-full h-full gap-6">
+                    <div className="flex flex-col justify-center items-center w-2/3 gap-y-4">
+                        <p className="text-[#F0F2F5] text-4xl lg:text-6xl font-extrabold">
+                            {featuresTitle}
+                        </p>
+                        <p className="text-[#F0F2F5] text-base lg:text-xl text-center md:w-2/3 lg:w-2/5">
+                            {featuresSub}
+                        </p>
+                    </div>
 
-                <div className='flex flex-row justify-center items-center px-4'>
-                    <img src={zapiIcon} className="w-80 p-2 hidden lg:flex" alt="Logo" />
-                    <div>
-                        <ul className="list-disc pl-6 text-[#F0F2F5]">
-                            {
-                                featuresList.map((f) => (
-                                    <li key={f.title} className="mb-2"> <b> {f.title} </b> {f.desc} </li>
-                                ))
-                            }
-                        </ul>
+                    <div className='flex flex-row justify-center items-center px-4'>
+                        <img src={zapiIcon} className="w-80 p-2 hidden lg:flex" alt="Logo" />
+                        <div>
+                            <ul className="list-disc pl-6 text-[#F0F2F5]">
+                                {
+                                    featuresList.map((f) => (
+                                        <li key={f.title} className="mb-2"> <b> {f.title} </b> {f.desc} </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
