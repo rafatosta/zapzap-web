f08a872647e3a33a03f72abff18b20f296ff7b3e
Rafael Tosta <rafa.ecomp@gmail.com>
2024-05-24 12:09:37 -0300

    Disable darkmode page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 
 export default {
+  darkMode: 'false',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
